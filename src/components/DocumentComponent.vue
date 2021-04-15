b5992f41e38b436ca078bf18e0d69d4875307593
Alejandro Millan <darkslave@live.com>
2021-04-15 16:23:50 -0500

    mobile landscape optimization

--- src/components/DocumentComponent.vue
+++ src/components/DocumentComponent.vue
@@ -39,7 +39,7 @@
         </div>
       </div>
       <div class="row">
-        <div class="col-lg-8">
+        <div class="col-lg-8 col-sm-8">
           <div class="text-subtitle2 w700 text-secondary">CLIENTE</div>
           <div class="text-h6 w700 text-dark">
             {{ data.clientData ? data.clientData.name : "" }}
@@ -51,7 +51,7 @@
             Email. {{ data.clientData ? data.clientData.email : "" }}
           </div>
         </div>
-        <div class="col-lg-4 text-right">
+        <div class="col-lg-4 col-sm-4 text-right">
           <q-separator />
           <div class="text-subitle2 w700 q-mt-sm text-secondary">Balance</div>
           <div class="text-h6 w700 text-dark">$ {{ calculateTotal }}</div>
@@ -65,7 +65,7 @@
         </div>
       </div>
       <div class="row text-center q-mt-lg q-mb-md">
-        <div class="col-lg-5 q-pr-md">
+        <div class="col-lg-5 col-sm-5 q-pr-md">
           <div
             class="text-subtitle2 w700 bg-dark text-white q-py-xs table-header"
           >
@@ -73,21 +73,21 @@
           </div>
         </div>
         <q-space />
-        <div class="col-lg-2 q-px-md">
+        <div class="col-lg-2 col-sm-2 q-px-md">
           <div
             class="text-subtitle2 w700 bg-dark text-white q-py-xs table-header"
           >
             COSTO
           </div>
         </div>
-        <div class="col-lg-3 q-px-md">
+        <div class="col-lg-3 col-sm-3 q-px-md">
           <div
             class="text-subtitle2 w700 bg-dark text-white q-py-xs table-header"
           >
             CANTIDAD
           </div>
         </div>
-        <div class="col-lg-2 q-pl-md">
+        <div class="col-lg-2 col-sm-2 q-pl-md">
           <div
             class="text-subtitle2 w700 bg-secondary text-white q-py-xs table-header"
           >
@@ -96,24 +96,24 @@
         </div>
       </div>
       <div class="row q-mb-sm" v-for="(item, i) in data.items" :key="i">
-        <div class="col-lg-5">
+        <div class="col-lg-5 col-sm-5">
           <div class="text-subtitle2 w700">
             {{ item.name.toUpperCase() }}
           </div>
           <div class="text-body">{{ item.description }}</div>
         </div>
         <q-space />
-        <div class="col-lg-2 text-center">
+        <div class="col-lg-2 col-sm-2 text-center">
           <div class="text-subtitle2">
             $ {{ item.price == "" ? "" : parseFloat(item.price).toFixed(2) }}
           </div>
         </div>
-        <div class="col-lg-3 text-center">
+        <div class="col-lg-3 col-sm-3 text-center">
           <div class="text-subtitle2">
             {{ item.price == "" ? "" : item.amount }}
           </div>
         </div>
-        <div class="col-lg-2 text-center">
+        <div class="col-lg-2 col-sm-2 text-center">
           <div class="text-subtitle2">
             $
             {{
@@ -127,17 +127,17 @@
     </div>
     <q-separator class="q-my-lg bg-dark" style="height: 3px" />
     <div class="row middle-document q-mx-lg q-px-xl q-py-lg">
-      <div class="col-lg-6">
+      <div class="col-lg-6 col-sm-6">
         <div class="text-h6 w700">Total:</div>
       </div>
-      <div class="col-lg-6">
+      <div class="col-lg-6 col-sm-6">
         <div class="text-h6 w700 text-right">$ {{ calculateTotal }}</div>
       </div>
     </div>
     <div class="bg-grey-3 q-pb-lg">
       <div class="bottom-document q-mx-lg q-px-xl">
         <div class="row q-py-lg">
-          <div class="col-lg-6">
+          <div class="col-lg-6 col-sm-6">
             <div class="text-subtitle2 w700 q-mb-sm">INFORMACION DE PAGO</div>
             <div class="text-body">
               <span class="w700">No. Cuenta:</span> 04-72-98-558246-9
@@ -150,7 +150,7 @@
               <span class="w700">Banco:</span> Banco General
             </div>
           </div>
-          <div class="col-lg-6">
+          <div class="col-lg-6 col-sm-6">
             <div class="text-subtitle2 w700 q-mb-sm">NOTAS:</div>
             <div class="text-body">
               {{ data.notes }}
@@ -160,17 +160,17 @@
 
         <q-separator />
         <div class="row text-center q-py-lg">
-          <div class="col-lg-4">
+          <div class="col-lg-4 col-sm-4">
             <div class="text-caption w600">
               <q-icon name="r_phone" /> (507) 6480-6778
             </div>
           </div>
-          <div class="col-lg-4">
+          <div class="col-lg-4 col-sm-4">
             <div class="text-caption w600">
               <q-icon name="r_language" /> www.blueballoon.io
             </div>
           </div>
-          <div class="col-lg-4">
+          <div class="col-lg-4 col-sm-4">
             <div class="text-caption w600">
               <q-icon name="r_email" /> diego@blueballoon.io
             </div>
@@ -212,7 +212,9 @@ export default {
   },
   methods: {
     formatDate(format) {
-      return moment(this.data.date).locale("es").format(format);
+      return moment(this.data.date)
+        .locale("es")
+        .format(format);
     },
   },
   computed: {
