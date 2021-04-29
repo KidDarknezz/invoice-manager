c507e09f95cb184d4004b6365135255a8ba8a5a5
Alejandro Millan <darkslave@live.com>
2021-04-29 13:49:35 -0500

    views by layouts

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,36 @@
+<template>
+    <q-layout view="hHh lpR fFf">
+        <q-page-container>
+            <div class="row bg-grey-5 shadow-3">
+                <q-space />
+                <div class="col-lg-7 col-md-10 col-sm-10 q-py-sm">
+                    <div class="row">
+                        <div class="col-lg-1 col-md-2 col-sm-2 col-xs-2 flex flex-center">
+                            <q-img :src="require('@/assets/logo_2.webp')" style="width: 35px;" />
+                        </div>
+                        <div class="col-lg-6 col-xs-8 flex flex-center">
+                            <div class="text-h5 bb-font w700 full-width text-grey-9">
+                                Blue Balloon Inc.
+                            </div>
+                        </div>
+                        <q-space />
+                        <div class="col-lg-2 col-xs-2 flex flex-center">
+                            <q-btn
+                                label="Cerrar sesion"
+                                no-caps
+                                flat
+                                rounded
+                                class="w700 text-dark full-width"
+                                icon="login"
+                                btn-logout-home
+                                @click="$store.dispatch('auth/logoutUser')"
+                            />
+                        </div>
+                    </div>
+                </div>
+                <q-space />
+            </div>
+            <router-view />
+        </q-page-container>
+    </q-layout>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,34 +1,5 @@
 <template>
     <q-page>
-        <div class="row bg-grey-5 shadow-3">
-            <q-space />
-            <div class="col-lg-7 col-md-10 col-sm-10 q-py-sm">
-                <div class="row">
-                    <div class="col-lg-1 col-md-2 col-sm-2 col-xs-2 flex flex-center">
-                        <q-img :src="require('@/assets/logo_2.webp')" style="width: 35px;" />
-                    </div>
-                    <div class="col-lg-6 col-xs-8 flex flex-center">
-                        <div class="text-h5 bb-font w700 full-width text-grey-9">
-                            Blue Balloon Inc.
-                        </div>
-                    </div>
-                    <q-space />
-                    <div class="col-lg-2 col-xs-2 flex flex-center">
-                        <q-btn
-                            label="Cerrar sesion"
-                            no-caps
-                            flat
-                            rounded
-                            class="w700 text-dark full-width"
-                            icon="login"
-                            btn-logout-home
-                            @click="$store.dispatch('auth/logoutUser')"
-                        />
-                    </div>
-                </div>
-            </div>
-            <q-space />
-        </div>
         <div class="row">
             <q-space />
             <div class="col-lg-7 col-md-10 col-sm-10">
@@ -37,8 +8,8 @@
                 </div>
                 <div class="row">
                     <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 q-pa-md">
-                        <router-link to="/clients" style="text-decoration: none;">
-                            <div class="home-tile bg-primary q-pa-md rounded-borders">
+                        <router-link to="/quotes" style="text-decoration: none;">
+                            <div class="home-tile bg-primary q-pa-md rounded-borders shadow-3">
                                 <q-icon
                                     name="fas fa-file-alt"
                                     size="xl"
@@ -53,7 +24,7 @@
                     </div>
                     <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 q-pa-md">
                         <router-link to="/invoices" style="text-decoration: none;">
-                            <div class="home-tile bg-primary q-pa-md rounded-borders">
+                            <div class="home-tile bg-primary q-pa-md rounded-borders shadow-3">
                                 <q-icon
                                     name="fas fa-file-invoice-dollar"
                                     size="xl"
@@ -68,7 +39,7 @@
                     </div>
                     <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 q-pa-md">
                         <router-link to="/clients" style="text-decoration: none;">
-                            <div class="home-tile bg-primary q-pa-md rounded-borders">
+                            <div class="home-tile bg-primary q-pa-md rounded-borders shadow-3">
                                 <q-icon
                                     name="fas fa-user-tag"
                                     size="xl"
